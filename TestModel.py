53d99e8a7439721997bca198b310d6ee8115470c
JJLee2910 <jangjhinL02@gmail.com>
2024-05-24 22:35:47 +0800

    restructure image processing for model testing

--- TestModel.py
+++ TestModel.py
@@ -9,35 +9,38 @@ emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fearful", 3: "Happy", 4: "Neutra
 num_classes = 8
 emotion_model = create_model(num_classes=num_classes)
 
+def extract_features(image):
+    feature = np.array(image)
+    feature = feature.reshape(1, 48, 48, 1)
+    return feature / 255.0
+
 # load weights into new model
 emotion_model.load_weights("Opt_Model/model_v3.h5")
 print("Loaded model from disk")
 
+face_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
 # start the webcam feed
 cap = cv2.VideoCapture(0)
 
 while True:
     # Find haar cascade to draw bounding box around face
     ret, frame = cap.read()
-    frame = cv2.resize(frame, (1280, 720))
-    if not ret:
-        break
-    face_detector = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
-    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    # detect faces available on camera
-    num_faces = face_detector.detectMultiScale(gray_frame, scaleFactor=1.3, minNeighbors=5)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(frame, 1.3, 5)
 
     # take each face available on the camera and Preprocess it
-    for (x, y, w, h) in num_faces:
-        cv2.rectangle(frame, (x, y-50), (x+w, y+h+10), (0, 255, 0), 4)
-        roi_gray_frame = gray_frame[y:y + h, x:x + w]
-        cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray_frame, (48, 48)), -1), 0)
+    for (x, y, w, h) in faces:
+        roi_gray_frame = gray[y:y + h, x:x + w]
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 255, 0), 4)
+        image = cv2.resize(roi_gray_frame, (48, 48))
 
+        img = extract_features(image)
         # predict the emotions
-        emotion_prediction = emotion_model.predict(cropped_img)
+        emotion_prediction = emotion_model.predict(img)
         maxindex = int(np.argmax(emotion_prediction))
-        cv2.putText(frame, emotion_dict[maxindex], (x+5, y-20), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
+        emotion_prob = np.max(emotion_prediction)
+        emotion_text = f"{emotion_dict[maxindex]}: {emotion_prob:.2f}"
+        cv2.putText(frame, emotion_text, (x+5, y-20), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
 
     cv2.imshow('Emotion Detection', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
