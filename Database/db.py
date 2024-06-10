4bd5fff04a8e65b142cb8f270ef537de52316b0a
JJLee2910 <jangjhinL02@gmail.com>
2024-06-10 18:28:44 +0800

    update db class

--- Database/db.py
+++ Database/db.py
@@ -1,11 +1,5 @@
 from pymongo import MongoClient
 
-client = MongoClient("mongodb+srv://Lee:kaixin12@cluster0.vmgyyjq.mongodb.net/")
-
-db = client['FYP']
-collection = db['user']
-
-
 class Database:
     def __init__(self):
         self.client = MongoClient('mongodb+srv://Lee:kaixin12@cluster0.vmgyyjq.mongodb.net/')
