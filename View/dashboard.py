29f58aa3a22b8cdcd6f6ae5b81142a20fd3635a1
JJLee2910 <jangjhinL02@gmail.com>
2024-06-28 21:22:34 +0800

    added back the comment

--- View/dashboard.py
+++ View/dashboard.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'Login.ui'
+# Form implementation generated from reading ui file 'Dashboard.ui'
 #
 # Created by: PyQt5 UI code generator 5.15.10
 #
